1bbfd681bd318d4909da639e896a2d9935f15319
Tejas123-creator-beep <tejasjoshi2k4@gmail.com>
2025-09-19 07:44:06 +0530

    Initial commit - Wanderlust project

---
+++ app.js
@@ -0,0 +1,101 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose");
+const Listing = require("./models/listing");
+const path = require("path")
+const methodOverride = require("method-override")
+const ejsMate = require ("ejs-mate")
+
+const Mongo_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+
+main()
+.then(()=>{
+    console.log("connected to db")
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+async function main() {
+    await mongoose.connect(Mongo_URL)
+    
+}
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate)
+
+
+app.get("/",(req,res)=>{
+    res.send("HI! i am root ")
+});
+// index route
+
+app.get("/listings",async(req,res)=>{
+    const allListings=await Listing.find({})
+res.render("listings/index", { allListings });
+});
+//new route
+
+app.get("/listings/new",(req,res)=>{
+res.render("listings/new");
+})
+
+//show route
+
+app.get("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    const listing=await Listing.findById(id)
+res.render("listings/show", { listing });});
+
+//create route
+app.post("/listings",(req,res)=>{
+    let newListing = new Listing(req.body.listing);
+    newListing.save()
+    res.redirect("/listings");
+})
+
+//Edit route
+
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id} = req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit",{listing})
+
+})
+//update route
+
+app.put("/listings/:id", async (req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+//delete route
+
+app.delete("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndDelete (id);
+    res.redirect("/listings");
+})
+
+
+// app.get("/testlisting",async(req,res)=>{
+//     let newListing = new Listing({
+//         title:"Pan pacific",
+//         description:"luxurious 5 star hotel",
+//         price:2100,
+//         location:"jaipur,rajasthan",
+//         country:"India"
+//     })
+//     await newListing.save();
+//     console.log("sample was saved")
+//     res.send("successful testing")
+   
+// })
+
+app.listen(8080,()=>{
+    console.log("the server is listning to the port 8080")
+
+})
